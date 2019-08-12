49c9941c044a6ad110af4c74b41ffd5d3e3b28aa
呱汼 <junyuan.hjy@alibaba-inc.com>
2019-08-12 16:39:55 +0800

    update 3.6.7 linux sdk

--- run.sh
+++ run.sh
@@ -15,5 +15,15 @@ cp -rf ./libs/${SYSTEM_NAME}/x64/* .
 echo "cp libs success!"
 fi
 
+if [ "$SYSTEM_NAME" == "Linux" ];then
+cp -rf ./libs/${SYSTEM_NAME}/* .
+echo "cp libs success!"
+fi
+
+if [ "$SYSTEM_NAME" == "Darwin" ];then
+cp -rf ./libs/${SYSTEM_NAME}/* .
+echo "cp libs success!"
+fi
+
 javac -encoding utf-8 -d . ./src/main/java/com/alibaba/cloud/faceengine/*.java src/main/java/${DEMO}.java src/main/java/Utils.java 
-java -Dfile.encoding=utf-8 -Djava.library.path=./libs/${SYSTEM_NAME}/ -Djava.library.path=./ ${DEMO}
+java -Dfile.encoding=utf-8 -Djava.library.path=./ ${DEMO}
