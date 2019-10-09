ff57abd858a0cd1ff25355ca60b5a7397e6dbc79
呱汼 <junyuan.hjy@alibaba-inc.com>
2019-10-09 21:17:08 +0800

    update 3.13.0 linux sdk

--- src/main/java/com/alibaba/cloud/faceengine/FaceRegister.java
+++ src/main/java/com/alibaba/cloud/faceengine/FaceRegister.java
@@ -32,8 +32,16 @@ public class FaceRegister {
         return FaceRegisterJNI.registerPicture(mContext, groupId, image, person, featureName);
     }
 
+    public int registerPicture2(String groupName, Image image, Person person, String featureName) {
+        return FaceRegisterJNI.registerPicture2(mContext, groupName, image, person, featureName);
+    }
+
     public int deleteGroup(String groupId) {
         return FaceRegisterJNI.deleteGroup(mContext, groupId);
+    }
+
+    public int deleteGroupByName(String groupName) {
+        return FaceRegisterJNI.deleteGroupByName(mContext, groupName);
     }
 
     public boolean isGroupExist(String groupName) {
@@ -62,6 +70,14 @@ public class FaceRegister {
 
     public int deletePerson(String personId) {
         return FaceRegisterJNI.deletePerson(mContext, personId);
+    }
+
+    public int deletePersonByName(String groupId, String personName) {
+        return FaceRegisterJNI.deletePersonByName(mContext, groupId, personName);
+    }
+
+    public int deletePersonByName2(String groupName, String personName) {
+        return FaceRegisterJNI.deletePersonByName2(mContext, groupName, personName);
     }
 
     public int deleteAllPersons(String groupId) {
@@ -98,6 +114,18 @@ public class FaceRegister {
 
     public int deleteFeature(String featureId) {
         return FaceRegisterJNI.deleteFeature(mContext, featureId);
+    }
+
+    public int deleteFeatureByName(String personId, String featureName) {
+        return FaceRegisterJNI.deleteFeatureByName(mContext, personId, featureName);
+    }
+
+    public int deleteFeatureByName2(String groupId, String personName, String featureName) {
+        return FaceRegisterJNI.deleteFeatureByName2(mContext, groupId, personName, featureName);
+    }
+
+    public int deleteFeatureByName3(String groupName, String personName, String featureName) {
+        return FaceRegisterJNI.deleteFeatureByName3(mContext, groupName, personName, featureName);
     }
 
     public int deleteAllFeatures(String personId) {
